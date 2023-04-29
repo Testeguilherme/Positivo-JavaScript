c765c0eb3f0822c5a33776105a407f23c1ba3bfb
Guilherme Frias <124912613+Testeguilherme@users.noreply.github.com>
2023-04-28 21:03:22 -0300

    Add files via upload

---
+++ teste/Candidato.java
@@ -0,0 +1,42 @@
+package teste;
+
+public class Candidato {
+	
+	private String nome;
+	private String cpf;
+	String[] lista = new String[10];
+	
+	public Candidato() {
+	}
+	
+	public Candidato(String nome, String cpf, String[] lista) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.lista = lista;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String[] getLista() {
+		return lista;
+	}
+
+	public void setLista(String[] lista) {
+		this.lista = lista;
+	}
+	
+	
+
+}
